1b2bb2f837980b185ed83dbd53806492713c4431
mdeane84 <mdeane84@yahoo.com>
2019-11-27 19:43:00 -0800

    commented the Hamiltonian Algorithm of the Square Sum Problem.

--- SquareSumsProblem/src/com/markdeane/hamiltonian/HamiltonianAlgorithm.java
+++ SquareSumsProblem/src/com/markdeane/hamiltonian/HamiltonianAlgorithm.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class HamiltonianAlgorithm {
-	
+
 	private int degree, iterations=0;
 	private int[][] adjacencyMatrix;
 	private ArrayList<Integer> hamiltonianPath = new ArrayList<Integer>();
 	private Stack<Integer> stack = new Stack<Integer>();
-	
+
 	public HamiltonianAlgorithm(int[][] adjacencyMatrix) {
-		
+
 		this.adjacencyMatrix = adjacencyMatrix;
 		this.degree = adjacencyMatrix[0].length;
 	}
-	
+
+
+	//	Calls the recursive function and displays the result.
 	public void displayBruteForce() {
 		if(isFeasible()) {
 			int start = findStart();
 			//int start = 0;
 			System.out.println("Starting node: " + (start+1));
-			
+
 			if(start == 0) {
 				int root = start;
 				while(!findPath(root)) {
@@ -39,7 +41,7 @@ public class HamiltonianAlgorithm {
 					return;
 				}
 			}
-			
+
 			while(!stack.isEmpty()) {
 				hamiltonianPath.add(stack.pop());
 			}
@@ -49,13 +51,15 @@ public class HamiltonianAlgorithm {
 		}
 		System.out.println("No feasible solution found...");
 	}
-	
+
+	// The recursive function that calculates the hamiltonian findPath.
+	// Iterates through all possible paths using a Stack.
 	private boolean findPath(int position) {
-		
+
 		stack.push(position + 1);
 		System.out.println(stack.toString());
 		iterations++;
-		
+
 		for(int i = 0; i < degree; i++) {
 			if(adjacencyMatrix[position][i] == 1 && !stack.contains(i+1)) {
 				if(findPath(i)) {
@@ -63,33 +67,40 @@ public class HamiltonianAlgorithm {
 				}
 			}
 		}
-		
+
 		if(stack.size() >= degree) {
 			return true;
 		}
-		
+
 		stack.pop();
 		return false;
 	}
-	
+
+	// Finds a feasible starting point for the hamiltonian path. Returns
+	// the index of the first vertex with degree 1. If one cannot be found,
+	// it returns 0 as the starting index.
 	private int findStart() {
 		int connections = 0;
 		for(int i = 0; i < degree; i++) {
 			connections = 0;
-			
+
 			for(int j = 0; j < degree; j++) {
 				if(adjacencyMatrix[i][j] == 1) {
 					connections++;
 				}
 			}
-			
+
 			if(connections == 1) {
 				return i;
 			}
 		}
 		return 0;
 	}
-	
+
+
+	// A hamiltonian path may only exist if there are 2 or fewer
+	// endpoints (vertices in the graph that have exactly 1 edge
+	// connected to it. Reterns true if there are 2 or fewer endpoints.
 	private boolean isFeasible() {
 		int endpoints = 0;
 		int connections = 0;
@@ -109,7 +120,7 @@ public class HamiltonianAlgorithm {
 		}
 		return true;
 	}
-	
+
 	public int getIterations() {
 		return iterations;
 	}
